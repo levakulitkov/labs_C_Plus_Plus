9c0f09a5e79b467d31c4deaeb5031effd9cbae20
levakulitkov <levakulitkov@yandex.ru>
2020-05-14 12:47:54 +0500

    add TreeNode.h

---
+++ task3_1404/MyBinaryTree/TreeNode.h
@@ -0,0 +1,28 @@
+#ifndef TREENODE_H
+#define TREENODE_H
+
+
+class TreeNode
+{
+public:
+	int getData() { return this->data; }
+	void setData(const int data) { this->data = data; }
+	TreeNode* getLeft() { return this->left; }
+	void setLeft(TreeNode* TN) { this->left = TN; }
+	TreeNode* getRight() { return this->right; }
+	void setRight(TreeNode* TN) { this->right = TN; }
+
+	TreeNode(const int data = int(), TreeNode* left = nullptr, TreeNode* right = nullptr)
+	{
+		this->data = data;
+		this->left = left;
+		this->right = right;
+	}
+
+	int data;
+	TreeNode* left;
+	TreeNode* right;
+};
+
+
+#endif // TREENODE_H
