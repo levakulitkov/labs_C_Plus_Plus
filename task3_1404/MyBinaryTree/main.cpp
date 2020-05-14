e80ce90df5336a442a6e1ace03e0ffca6c376e70
levakulitkov <48909050+levakulitkov@users.noreply.github.com>
2020-05-14 12:45:30 +0500

    Update main.cpp

--- task3_1404/MyBinaryTree/main.cpp
+++ task3_1404/MyBinaryTree/main.cpp
@@ -1,6 +1,5 @@
-﻿#include <iostream>
+#include <iostream>
 #include "BinaryTree.h"
-
 
 
 int main()
@@ -8,7 +7,7 @@ int main()
 	setlocale(LC_ALL, "ru");
 
 	BinaryTree Tree;
-	int Arr[] = {12, 23, 312, 412, 412, 6347, 745, 7457, 8887, 8888};
-	Tree.CreateMinimalBST(Arr, 3, 9);
+	int Arr[] = {2, 4, 5, 7, 9, 12, 23, 312, 412, 745, 6347, 7457, 8887, 8888};
+	Tree.CreateMinimalBST(Arr, 2, 13);
 	return 0;
 }
